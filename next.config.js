e36d8bdf5eddb193f1cf4118f951e4ea009d865b
Felipe Rodrigues <fdaldegan@gmail.com>
2023-03-02 16:56:22 -0300

    Adicionando novo método de "metadata"

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
 
-module.exports = nextConfig
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: [{ loader: "@svgr/webpack", }],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
